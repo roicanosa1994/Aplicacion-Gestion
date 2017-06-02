84f3fe544e153d8282324532605d02a3a4e8fcf0
roicanosa1994 <roicanosa3011@gmail.com>
2017-06-02 20:09:15 +0200

    Add files via upload

---
+++ privacidad/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta  charset="windows-1252">
+  <title> Pólitica de privacidad</title>
+  <meta name="author"  content="Aplicacion Proteccion Civil">
+  <style>
+  .subtitular {
+    font-family: 'Vidaloka', serif;
+    font-weight: bold;
+    text-align: left;
+    font-size: 1.1em;
+    margin-bottom: 10px;
+    display: block;
+}
+
+.titular {
+    font-family: 'Vidaloka', serif;
+    font-size: 1.7em;
+    font-weight: bold;
+    margin-bottom: 20px;
+    color: #006699;
+    text-align: left;
+    display: block;
+}
+#contenedor {
+    width: 90%;
+    max-width: 1280px;
+}
+  </style>
+</head>
+<body>
+  <div id="contenedor">
+    <div class="titular">Politica de privacidad</div>
+    <div style="font-size:1eM">
+      <P>
+      </P>
+      <div class="subtitular">Envío de comunicaciones a través de medios electrónicos.</div>
+      <br>
+      <br>
+      <br>
+  <p>Los mensajes enviados a través de correos electrónicos o formularios web son mensajes que se dirigen exclusivamente a su destinatario y pueden contener información privilegiada o confidencial. Si no es vd. el destinatario indicado, queda notificado de que la utilización, divulgación y/o copia sin autorización está prohibida en virtud de la legislación vigente.
+</p>
+  <div class="subtitular">Aviso importante (comunicaciones comerciales).</div>
+  <p>De conformidad con lo establecido en la Ley 34/2002 de 11 de julio, de Servicios de la Sociedad de la Información y del Comercio Electrónico, y la Directiva 2002/58/CE le comunicamos que en el supuesto de que no desee recibir comunicaciones e informaciones de índole comercial mediante este sistema de comunicación electrónica, nos lo indique por esta misma vía indicando en el asunto “BAJA COMUNICACIONES COMERCIALES” para que sus datos personales sean dados de baja de nuestra base de datos. Su solicitud será accionada en un plazo de 10 días desde su envío. En el supuesto de que no recibamos contestación expresa por su parte, entenderemos que acepta y autoriza que nuestra empresa siga realizando las referidas comunicaciones.
+</p>
+<br>
+<br>
+<br>
+<div class="subtitular">Protección de datos.</div>
+<br>
+<br>
+<p>a) Incorporación de datos personales a ficheros y su responsable.
+
+Se informa a los titulares de datos personales que los datos que nos facilite, a través de cualquiera de nuestros medios de recogida de información, van a ser introducidos indistintamente en ficheros parcialmente automatizados, titularidad del siguiente Responsable del Fichero:
+
+<br>
+<br>
+<br>
+
+AGRUPACION DE VOLUNTARIOS DE PROTECCIÓN CIVIL DE CULLEREDO
+<br>
+<br>
+<br>
+
+
+
+El Responsable del Fichero es plenamente consciente del uso y tratamiento que se le deben dar a los datos de carácter personal, y en virtud de la normativa legal existente al respecto (en particular la Ley Orgánica 15/1999, de 13 de diciembre de Protección de Datos de Carácter Personal.- en adelante LOPD- y al Real Decreto 1720/2007 del 21 de diciembre que la desarrolla) tiene implantado un sistema para preservar la seguridad, integridad y confidencialidad de los datos de carácter personal.
+Estos datos se tratarán para alguna de las siguientes finalidades: Gestión de contactos con alumnos/padres/tutores legales, cobro de cuotas, cumplimiento de obligaciones contables y fiscales, gestión laboral y otras gestiones con los trabajadores, gestiones informativas y de eventos.
+
+
+b) Derechos que asisten al titular y forma de ejercicio.
+
+El Responsable del Fichero informa que, en virtud de lo dispuesto en la legislación aprobada al respecto, podrá hacer valer sus derechos de: Acceso, Cancelación, Rectificación y, en su caso, Oposición al tratamiento de los datos personales de los que son titulares mediante un escrito (adjuntando fotocopia DNI) dirigido a:</p>
+</body>
+</html>
